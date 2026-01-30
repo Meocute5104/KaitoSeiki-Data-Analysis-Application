6df950a83fb8472fef29fc7ae6cc5e0f123a4891
Meocute5104 <nguyendangkhoa5104@gmail.com>
2026-01-30 13:37:50 +0900

    Update bar chat

--- charts/bar.py
+++ charts/bar.py
@@ -2,6 +2,6 @@ import plotly.express as px
 
 def process_bar(df, processes, std_total, title):
     fig = px.bar(df, x="SP", y=processes, title=title)
-    fig.add_hline(y=std_total, line_dash="dash", line_color="red", annotation_text="標準時間")
+    fig.add_hline(y=std_total, line_dash="dash", line_color="red", annotation_text="標準工数")
     fig.update_xaxes(type="category")
     return fig
